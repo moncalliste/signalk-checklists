7e55708b2e0e895700384c4411aca449538f32fb
Kent Ibbetson <kent.ibbetson@moncalliste.com>
2025-01-04 17:30:34 +0000

    Get some data from internal signalk store

--- src/components/AppPanel.tsx
+++ src/components/AppPanel.tsx
@@ -8,6 +8,13 @@ const AppPanel = (props: any) => {
     return <props.adminUI.Login />;
   }
 
+  fetch(
+    "/signalk/v1/applicationData/global/signalk_checklists/1.0.0/checklists"
+  )
+    .then((response) => response.json)
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+
   return <div>testing</div>;
 };
 export default AppPanel;
